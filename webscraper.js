062045a987a9ec598d9f90ad42a37bb9fe2fe66c
Oruma1to <60720664+Oruma1to@users.noreply.github.com>
2021-07-01 17:31:41 -0400

    first commit

--- webscraper.js
+++ webscraper.js
@@ -1,29 +1,32 @@
 const axios = require('axios')
 const cheerio = require('cheerio')
-const prompt = require('prompt-sync')({sigint: true})
+const prompt = require('prompt-sync')({ sigint: true })
 
 const runScraper = async () => {
   const new_url = prompt('Enter a URL: ')
-  console.log(`Now grabbing: ${new_url}`)
-  url = new_url ? new_url : `https://www.youtube.com/watch?v=QRMvvhiHnk8`
-  const response = await axios.get(url)
+  let url = new_url
+  if (url.includes('youtube.com')) {
+    console.log(`Now grabbing: ${new_url}`)
+    let keywords = []
+    const response = await axios.get(url)
+    const $ = cheerio.load(response.data)
+    $('meta').each((i, el) => {
+      if ($(el).attr('name') === 'keywords') {
+        keywords = $(el).attr('content')
+      }
+    })
+    console.log('<-------------------------------------------->')
+    keywords.split(',').forEach((keyword, idx) => {
+      console.log(idx + 1, keyword)
+    })
+    // console.log("List of keywords: " + keywords)
+    console.log(`Total number of keywords: ${keywords.split(', ').length}`)
+    console.log(`Total string length: ${keywords.length}`)
 
-  const $ = cheerio.load(response.data)
-
-  let keywords = []
-
-  // console.log(keywords)
-  $('meta').each((i, el) => {
-    if ($(el).attr('name') === 'keywords') {
-      keywords = $(el).attr('content')
-    }
-  })
-
-  console.log(keywords)
-  console.log(`Total number of keywords: ${keywords.split(', ').length}`)
-  console.log(`Total string length: ${keywords.length}`)
-  
-
+  } else {
+    console.log('Please enter a valid youtube url')
+    runScraper()
+  }
 }
 
 runScraper()
